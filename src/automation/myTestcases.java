e67497340206028eed0c5d37948260991195a79f
Sda0177381 <alnassershatha0@gmail.com>
2025-09-14 22:03:36 +0300

    update

---
+++ src/automation/myTestcases.java
@@ -0,0 +1,19 @@
+package automation;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class myTestcases {
+	
+	
+	
+	@BeforeTest
+	
+	public void mySetup() {}
+	
+	
+	@Test
+	
+	public void myTest() {}
+
+}
